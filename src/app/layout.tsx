1731c126c25a24487047df6fffbd334117a95557
nuny0831 <yrch92@gmail.com>
2024-01-09 15:36:54 +0900

    chore: 폴더구조 및 쿼리설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import './variables.css';
+import ReactQueryProviders from '@/utils/ReactQueryProviders';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,8 +14,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReactQueryProviders>{children}</ReactQueryProviders>
+      </body>
     </html>
   );
 }
-
