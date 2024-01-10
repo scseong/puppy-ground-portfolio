d8fc9201fe3dc26969c7ce489f90593222913133
nuny0831 <yrch92@gmail.com>
2024-01-10 14:23:02 +0900

    feat: layout UI, 페이지 연결 (#3)

--- src/app/_components/layout/Footer.tsx
+++ src/app/_components/layout/Footer.tsx
@@ -1,7 +1,11 @@
+import styles from './footer.module.scss';
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={styles.footerBox}>
+      Copyright 2023 &copy; <strong>Puppy Ground</strong> All rights reserved.
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/app/_components/layout/Header.tsx
+++ src/app/_components/layout/Header.tsx
@@ -1,7 +1,44 @@
+import Link from 'next/link';
+import styles from './header.module.scss';
+import Image from 'next/image';
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className={styles.navbarBox}>
+      <div className={styles.logoBox}>
+        <Image src="/logo.png" alt="logo" width={90} height={60} />
+        <Link href={`/`} className={styles.logoText}>
+          Puppy Ground
+        </Link>
+      </div>
+      <div className={styles.menuBox}>
+        <Link className={styles.menuItem} href={`/used-goods`}>
+          중고거래
+        </Link>
+        <Link className={styles.menuItem} href={`/stray-dogs`}>
+          유기견
+        </Link>
+        <Link className={styles.menuItem} href={`/facilities`}>
+          동반시설
+        </Link>
+        <Link className={styles.menuItem} href={`/mungstagram`}>
+          멍스타그램
+        </Link>
+        <div className={styles.menuItem}>채팅</div>
+        <div className={styles.menuItem}>알람</div>
+        <Link className={styles.menuItem} href={`/profile`}>
+          마이페이지
+        </Link>
+        <Link className={styles.menuItem} href={`/auth/signup`}>
+          회원가입
+        </Link>
+        <Link className={styles.menuItem} href={`/auth/login`}>
+          로그인
+        </Link>
+        <div className={styles.menuItem}>로그아웃</div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
