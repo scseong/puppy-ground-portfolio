6ccc3c8382800323326fec695e7aa67a0a490317
scseong <goidtv@naver.com>
2024-01-18 08:37:44 +0900

    feat: 판매완료, 판매중으로 구분하여 목록 표시 (#9)

--- src/app/(providers)/used-goods/_components/UsedGoodsList.tsx
+++ src/app/(providers)/used-goods/_components/UsedGoodsList.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import UsedGoodsItem from './UsedGoodsItem';
 import { SearchParams, getQueryFunction, getQueryKey } from '../page';
@@ -7,6 +8,7 @@ import { useSearchParams } from 'next/navigation';
 import { handleSearch } from '../actions';
 
 const UsedGoodsList = () => {
+  const [isSoldOut, setSoldOut] = useState(false);
   const router = useSearchParams();
   const params: SearchParams = Object.fromEntries(router);
   const { data } = useQuery({
@@ -17,14 +19,28 @@ const UsedGoodsList = () => {
   // TODO: Empty Component 만들기
   if (!data) return <div>상품이 없습니다.</div>;
 
+  const filtedGoods = data.filter((goods) => goods.sold_out === isSoldOut);
+
+  const handleSoldOut = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const name = e.currentTarget.name;
+    if (name === 'soldout') return setSoldOut(true);
+    setSoldOut(false);
+  };
+
   return (
     <>
       <form action={handleSearch}>
         <input type="text" name="query" />
         <button>검색</button>
       </form>
+      <button name="avaliable" onClick={handleSoldOut}>
+        판매중
+      </button>
+      <button name="soldout" onClick={handleSoldOut}>
+        판매 완료
+      </button>
 
-      {data?.map((goods) => <UsedGoodsItem key={goods.id} goods={goods} />)}
+      {filtedGoods?.map((goods) => <UsedGoodsItem key={goods.id} goods={goods} />)}
     </>
   );
 };
