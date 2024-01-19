facd6a44e63e4d9dafd027d806f728b17e308c7e
scseong <goidtv@naver.com>
2024-01-19 14:12:07 +0900

    design: react-slect 라이브러리 사용한 카테고리 UI (#44)

--- src/app/(providers)/used-goods/_components/UsedGoodsFilter.tsx
+++ src/app/(providers)/used-goods/_components/UsedGoodsFilter.tsx
@@ -1,10 +1,10 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { supabase } from '@/shared/supabase/supabase';
 import { useQuery } from '@tanstack/react-query';
 import styles from './usedGoodsFilter.module.scss';
-import Dropdown from './Dropdown';
+import Select, { StylesConfig } from 'react-select';
 
 type CategoryObject = {
   mainCategory: string[];
@@ -31,8 +31,71 @@ const getCategories = async (): Promise<CategoryObject> => {
   }, {} as CategoryObject);
 };
 
+const main = [
+  { value: '1', label: '대형견' },
+  { value: '2', label: '중형견' },
+  { value: '3', label: '소형견' }
+];
+
+const sub = [
+  { value: '1', label: '장난감' },
+  { value: '2', label: '식품' },
+  { value: '3', label: '의류' },
+  { value: '4', label: '기타' }
+];
+
+type Category = typeof main;
+export interface ColourOption {
+  readonly value: string;
+  readonly label: string;
+  readonly color: string;
+  readonly isFixed?: boolean;
+  readonly isDisabled?: boolean;
+}
+
+const colourStyles: StylesConfig<{ value: string; label: string }, true> = {
+  option: (styles) => {
+    return {
+      ...styles,
+      fontSize: '0.8rem'
+    };
+  },
+  multiValue: (styles) => {
+    return {
+      ...styles,
+      backgroundColor: '#0ac4b9',
+      borderRadius: '15px',
+      padding: '5px',
+      color: 'white'
+    };
+  },
+  multiValueLabel: (styles) => {
+    return {
+      ...styles,
+      color: 'white'
+    };
+  },
+  clearIndicator: (styles) => {
+    return {
+      ...styles,
+      display: 'none'
+    };
+  },
+  multiValueRemove: (styles) => {
+    return {
+      ...styles,
+      backgroundColor: 'inherit',
+      ':hover': {
+        backgroundColor: 'inherit',
+        cursor: 'pointer'
+      }
+    };
+  }
+};
+
 const UsedGoodsFilter = () => {
-  const [selected, setSelected] = useState({ main: new Set(), sub: new Set() });
+  const [selectedMain, setSelectedMain] = useState<Category>([]);
+  const [selectedSub, setSelectedSub] = useState<Category>([]);
   const { data } = useQuery<CategoryObject>({ queryKey: ['categories'], queryFn: getCategories });
 
   if (!data) return;
@@ -40,8 +103,28 @@ const UsedGoodsFilter = () => {
   return (
     // TODO: css 분리
     <div className={styles.wrapper}>
-      <Dropdown categories={data.mainCategory} defaultText="견종 크기" />
-      <Dropdown categories={data.subCategory} defaultText="카테고리" />
+      <Select
+        className={styles.customSelect}
+        defaultValue={selectedMain}
+        onChange={(newValue) => {
+          setSelectedMain([...newValue]);
+        }}
+        options={main}
+        placeholder="견종 사이즈"
+        styles={colourStyles}
+        isMulti
+      />
+      <Select
+        className={styles.customSelect}
+        defaultValue={selectedSub}
+        onChange={(newValue) => {
+          setSelectedSub([...newValue]);
+        }}
+        options={sub}
+        placeholder="카테고리"
+        styles={colourStyles}
+        isMulti
+      />
     </div>
   );
 };
