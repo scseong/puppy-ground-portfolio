b51b6acabe99a437d9bff5db0ee5c15e46df4182
scseong <goidtv@naver.com>
2024-01-23 19:18:33 +0900

    feat: 멍스타그램 페이지 라우팅 (#75)

---
+++ src/app/(providers)/mungstagram/[id]/update/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>게시글 수정</div>;
+};
+
+export default page;
--- src/app/(providers)/mungstagram/page.tsx
+++ src/app/(providers)/mungstagram/page.tsx
@@ -1,5 +1,20 @@
+import Link from 'next/link';
+
 const page = () => {
-  return <div>멍스타그램</div>;
+  return (
+    <div>
+      <h2>멍스타그램</h2>
+      <div>
+        <Link href="/mungstagram/1">게시글로 이동</Link>
+      </div>
+      <div>
+        <Link href="/mungstagram/create">글쓰기</Link>
+      </div>
+      <div>
+        <Link href="/mungstagram/1/update">수정하기</Link>
+      </div>
+    </div>
+  );
 };
 
 export default page;
