01e27c5efcf83f5f4bbb3ff5f84da2ede33d535a
nuny0831 <yrch92@gmail.com>
2024-01-27 01:09:58 +0900

    feat/design: 마이페이지 리스트 UI 및 관심상품 불러오기(#83)

---
+++ src/app/(providers)/profile/_components/Card.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image';
+import styles from './card.module.scss';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import { addCommasToNumber } from '@/utils/format';
+import { getStringFromNow } from '@/utils/time';
+import { getCountFromTable } from '@/utils/table';
+
+type wishGoodsProps = {
+  goods: {
+    id: number;
+    created_at: string;
+    title: string;
+    price: number;
+    address: string;
+    sold_out: boolean;
+    photo_url: string[];
+    used_item_wish: object;
+    chat_list: object;
+  };
+};
+
+const Card = ({ goods }: wishGoodsProps) => {
+  const { id, created_at, title, photo_url, sold_out, address, price, used_item_wish, chat_list } =
+    goods;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.image}>
+        <Image src={photo_url[0]} alt="상품 이미지" width={250} height={250} />
+        {sold_out && <div className={styles.overlay}></div>}
+        <div className={styles.info}>
+          <h3>{title}</h3>
+          <div className={styles.detail}>
+            <span>{addCommasToNumber(price)}원</span>
+            <time>{getStringFromNow(created_at)}</time>
+          </div>
+          <div className={styles.address}>
+            <FaMapMarkerAlt />
+            <address>{address}</address>
+          </div>
+          <div className={styles.count}>
+            <span>찜 {getCountFromTable(used_item_wish)}개</span>
+            <span className={styles.divide}></span>
+            <span>채팅 {getCountFromTable(chat_list)}개</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/app/(providers)/profile/_components/List.tsx
+++ src/app/(providers)/profile/_components/List.tsx
@@ -1,7 +1,39 @@
-import styles from './List.module.scss';
+'use client';
+import Card from './Card';
+import styles from './list.module.scss';
+import useAuth from '@/hooks/useAuth';
+import { getUsedGoodWish } from '@/apis/wishLike/actions';
+import { useQuery } from '@tanstack/react-query';
 
 const List = () => {
-  return <div className={styles.container}>리스트가 들어갈 자리</div>;
+  const user = useAuth((state) => state.user);
+
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ['wish-list'],
+    queryFn: () => getUsedGoodWish(user!.id),
+    enabled: !!user,
+    select: (res) => res?.map((value) => value.used_item)
+  });
+
+  console.log(data);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <h2>마이페이지</h2>
+        <button>알림 설정</button>
+      </div>
+      <div className={styles.tab}>
+        <button>관심상품</button>
+        <button>등록한 상품</button>
+      </div>
+      <div className={styles.cardContainer}>
+        <div className={styles.cardWrapper}>
+          {data?.map((goods) => (goods ? <Card key={goods.id} goods={goods} /> : undefined))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default List;
