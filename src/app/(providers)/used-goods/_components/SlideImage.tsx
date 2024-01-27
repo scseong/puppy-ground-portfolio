8f6f952b312b3dc896f0b1628f2b87ba64a3aafd
scseong <goidtv@naver.com>
2024-01-27 23:49:29 +0900

    feat: 슬라이드 이미지 마우스 드래그로 전환

--- src/app/(providers)/used-goods/_components/SlideImage.tsx
+++ src/app/(providers)/used-goods/_components/SlideImage.tsx
@@ -8,57 +8,105 @@ import styles from './slideImage.module.scss';
 
 const cx = cn.bind(styles);
 
-const SlideImage = ({ images }: { images: string[] }) => {
-  const [currentIdx, setCurrentIdx] = useState(0);
-
-  const onPrevClick = () => {
-    if (currentIdx > 0) setCurrentIdx((prev) => prev - 1);
-  };
-  const onNextClick = () => {
-    if (currentIdx < images.length - 1) setCurrentIdx((prev) => prev + 1);
-  };
-
-  const onDotClick = (idx: number) => setCurrentIdx(idx);
-
-  const slideVariants = {
-    visible: {
+const variants = {
+  enter: (direction: number) => {
+    return {
+      x: direction > 0 ? 500 : -500,
       opacity: 0
-    },
-    exit: {
-      opacity: 1
-    }
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 500 : -500,
+      opacity: 0
+    };
+  }
+};
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset: number, velocity: number) => {
+  return Math.abs(offset) * velocity;
+};
+
+const wrap = (min: number, max: number, v: number) => {
+  const rangeSize = max - min;
+  return ((((v - min) % rangeSize) + rangeSize) % rangeSize) + min;
+};
+
+const SlideImage = ({
+  images,
+  sizes
+}: {
+  images: string[];
+  sizes?: React.CSSProperties;
+}) => {
+  const [[page, direction], setPage] = useState([0, 0]);
+  const imageIndex = wrap(0, images.length, page);
+
+  const paginate = (newDirection: number) => {
+    setPage([page + newDirection, newDirection]);
   };
 
   return (
-    // TODO: layout shift issue
-    <div className={styles.wrapper}>
-      <MotionConfig transition={{ duration: 0.6 }}>
+    <div className={styles.wrapper} style={sizes}>
+      <MotionConfig transition={{ duration: 0.4 }}>
         <motion.div>
-          <AnimatePresence>
-            <motion.img
-              key={currentIdx}
-              src={images[currentIdx]}
-              alt={images[currentIdx]}
-              variants={slideVariants}
-              initial="visible"
-              animate="exit"
-              style={{ width: '400px', height: '400px' }}
-            />
+          <AnimatePresence initial={false} custom={direction}>
+            {images.length > 1 && (
+              <motion.img
+                key={page}
+                src={images[imageIndex]}
+                custom={direction}
+                variants={variants}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                transition={{
+                  x: { type: 'spring', stiffness: 300, damping: 30 },
+                  opacity: { duration: 0.2 }
+                }}
+                drag="x"
+                dragConstraints={{ left: 0, right: 0 }}
+                dragElastic={1}
+                onDragEnd={(e, { offset, velocity }) => {
+                  const swipe = swipePower(offset.x, velocity.x);
+
+                  if (swipe < -swipeConfidenceThreshold) {
+                    paginate(1);
+                  } else if (swipe > swipeConfidenceThreshold) {
+                    paginate(-1);
+                  }
+                }}
+                style={sizes}
+              />
+            )}
+            {images.length === 1 && (
+              <motion.img key={imageIndex} src={images[imageIndex]} alt={images[imageIndex]} style={sizes}/>
+            )}
           </AnimatePresence>
         </motion.div>
-        <motion.div className={cx('directionBtn', 'leftBtn')} onClick={onPrevClick}>
-          <GoChevronLeft size="2rem" />
-        </motion.div>
-        <motion.div className={cx('directionBtn', 'rightBtn')} onClick={onNextClick}>
-          <GoChevronRight size="2rem" />
-        </motion.div>
-        <div className={styles.indicator}>
-          {images.map((_, idx) => (
-            <motion.div key={idx} onClick={() => setCurrentIdx(idx)}>
-              {currentIdx === idx ? <GoDotFill /> : <GoDot />}
+        {images.length > 1 && (
+          <>
+            <motion.div className={cx('directionBtn', 'leftBtn')} onClick={() => paginate(-1)}>
+              <GoChevronLeft size="1.2rem" />
             </motion.div>
-          ))}
-        </div>
+            <motion.div className={cx('directionBtn', 'rightBtn')} onClick={() => paginate(1)}>
+              <GoChevronRight size="1.2rem" />
+            </motion.div>
+            <div className={styles.indicator}>
+              {images.map((_, idx) => (
+                <motion.div key={idx} onClick={() => setPage([idx, 0])}>
+                  {imageIndex === idx ? <GoDotFill /> : <GoDot />}
+                </motion.div>
+              ))}
+            </div>
+          </>
+        )}
       </MotionConfig>
     </div>
   );
