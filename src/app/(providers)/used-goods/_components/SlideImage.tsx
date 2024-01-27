1c87a3577483e1e0d61849067999809b042c08a2
scseong <goidtv@naver.com>
2024-01-28 04:22:21 +0900

    chore: 이미지 슬라이더 버튼 크기 및 투명도 조절

--- src/app/(providers)/used-goods/_components/SlideImage.tsx
+++ src/app/(providers)/used-goods/_components/SlideImage.tsx
@@ -38,13 +38,7 @@ const wrap = (min: number, max: number, v: number) => {
   return ((((v - min) % rangeSize) + rangeSize) % rangeSize) + min;
 };
 
-const SlideImage = ({
-  images,
-  sizes
-}: {
-  images: string[];
-  sizes?: React.CSSProperties;
-}) => {
+const SlideImage = ({ images, sizes }: { images: string[]; sizes?: React.CSSProperties }) => {
   const [[page, direction], setPage] = useState([0, 0]);
   const imageIndex = wrap(0, images.length, page);
 
@@ -55,53 +49,58 @@ const SlideImage = ({
   return (
     <div className={styles.wrapper} style={sizes}>
       <MotionConfig transition={{ duration: 0.4 }}>
-        <motion.div>
-          <AnimatePresence initial={false} custom={direction}>
-            {images.length > 1 && (
-              <motion.img
-                key={page}
-                src={images[imageIndex]}
-                custom={direction}
-                variants={variants}
-                initial="enter"
-                animate="center"
-                exit="exit"
-                transition={{
-                  x: { type: 'spring', stiffness: 300, damping: 30 },
-                  opacity: { duration: 0.2 }
-                }}
-                drag="x"
-                dragConstraints={{ left: 0, right: 0 }}
-                dragElastic={1}
-                onDragEnd={(e, { offset, velocity }) => {
-                  const swipe = swipePower(offset.x, velocity.x);
+        {/* <motion.div> */}
+        <AnimatePresence initial={false} custom={direction}>
+          {images.length > 1 && (
+            <motion.img
+              key={page}
+              src={images[imageIndex]}
+              custom={direction}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{
+                x: { type: 'spring', stiffness: 300, damping: 30 },
+                opacity: { duration: 0.2 }
+              }}
+              drag="x"
+              dragConstraints={{ left: 0, right: 0 }}
+              dragElastic={1}
+              onDragEnd={(e, { offset, velocity }) => {
+                const swipe = swipePower(offset.x, velocity.x);
 
-                  if (swipe < -swipeConfidenceThreshold) {
-                    paginate(1);
-                  } else if (swipe > swipeConfidenceThreshold) {
-                    paginate(-1);
-                  }
-                }}
-                style={sizes}
-              />
-            )}
-            {images.length === 1 && (
-              <motion.img key={imageIndex} src={images[imageIndex]} alt={images[imageIndex]} style={sizes}/>
-            )}
-          </AnimatePresence>
-        </motion.div>
+                if (swipe < -swipeConfidenceThreshold) {
+                  paginate(1);
+                } else if (swipe > swipeConfidenceThreshold) {
+                  paginate(-1);
+                }
+              }}
+              style={sizes}
+            />
+          )}
+          {images.length === 1 && (
+            <motion.img
+              key={imageIndex}
+              src={images[imageIndex]}
+              alt={images[imageIndex]}
+              style={sizes}
+            />
+          )}
+        </AnimatePresence>
+        {/* </motion.div> */}
         {images.length > 1 && (
           <>
             <motion.div className={cx('directionBtn', 'leftBtn')} onClick={() => paginate(-1)}>
-              <GoChevronLeft size="1.2rem" />
+              <GoChevronLeft size="2.4rem" />
             </motion.div>
             <motion.div className={cx('directionBtn', 'rightBtn')} onClick={() => paginate(1)}>
-              <GoChevronRight size="1.2rem" />
+              <GoChevronRight size="2.4rem" />
             </motion.div>
             <div className={styles.indicator}>
               {images.map((_, idx) => (
                 <motion.div key={idx} onClick={() => setPage([idx, 0])}>
-                  {imageIndex === idx ? <GoDotFill /> : <GoDot />}
+                  {imageIndex === idx ? <GoDotFill  /> : <GoDot />}
                 </motion.div>
               ))}
             </div>
