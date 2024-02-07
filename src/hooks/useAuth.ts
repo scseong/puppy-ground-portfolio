2df78689f41346c3810d7b697fd27696bb9ddf1c
jiwon son <juwon062955@gmail.com>
2024-02-07 11:49:46 +0900

    chore: 유저정보에 따른 헤더 변경 로직 수정(#196)

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,19 +3,15 @@ import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
 export type AuthStore = {
-  isAuthInitialized: boolean;
   user: User | null;
   setUser: (user: User | null) => void;
-  setIsAuthInitialized: (isAuthInitialized: boolean) => void;
 };
 
 const useAuth = create<AuthStore>()(
   persist(
     (set) => ({
-      isAuthInitialized: false,
       user: null,
-      setUser: (user) => set(() => ({ user })),
-      setIsAuthInitialized: (isAuthInitialized) => set(() => ({ isAuthInitialized }))
+      setUser: (user) => set(() => ({ user }))
     }),
     {
       name: 'user-store',
