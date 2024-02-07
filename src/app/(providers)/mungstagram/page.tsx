65c14e2f92a2737322497ed848c28fc15963a022
scseong <goidtv@naver.com>
2024-02-07 20:34:15 +0900

    fix: 멍스타그램 쿼리키 오탈자 수정 (#201)

--- src/app/(providers)/mungstagram/page.tsx
+++ src/app/(providers)/mungstagram/page.tsx
@@ -2,20 +2,31 @@ import { getMungstaPosts } from '@/apis/mung-stagram/action';
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
 import getQueryClient from '@/app/_components/lib/getQueryClient';
 import MungstaPosts from './_components/root/MungstaPosts';
+import { Main } from '@/app/_components/layout';
+import Link from 'next/link';
+import styles from './page.module.scss';
 
 export const revalidate = 0;
 
 const MungstaMainPage = async () => {
   const queryClient = getQueryClient();
   await queryClient.prefetchQuery({
-    queryKey: ['munstagram'],
+    queryKey: ['mungstagram'],
     queryFn: getMungstaPosts
   });
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <MungstaPosts />
-    </HydrationBoundary>
+    <Main>
+      <div className={styles.header}>
+        <h2>멍스타그램</h2>
+        <Link href="/mungstagram/create">등록하기</Link>
+      </div>
+      <section className={styles.mungstagram}>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <MungstaPosts />
+        </HydrationBoundary>
+      </section>
+    </Main>
   );
 };
 
