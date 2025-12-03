0f67afe0d2e79ffe3e54262f1c0fa88c74bb9f03
scseong <goidtv@naver.com>
2025-12-03 21:46:12 +0900

    chore: 로딩 Image 컴포넌트에서 video로 변경

--- src/app/_components/layout/loading/Loading.tsx
+++ src/app/_components/layout/loading/Loading.tsx
@@ -1,12 +1,19 @@
-import Image from 'next/image';
 import React from 'react';
-import loadingBar from '../../../../../public/images/loader.gif';
 import styles from './loading.module.scss';
 
 const Loading = () => {
   return (
     <div className={styles.container}>
-      <Image src={loadingBar} className={styles.image} alt="loading" quality={30} priority />
+      <video
+        src="/images/loader.mp4"
+        className={styles.image}
+        width="350"
+        height="350"
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
     </div>
   );
 };
