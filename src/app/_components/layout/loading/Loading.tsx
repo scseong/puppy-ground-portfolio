b8d482e41ddf497cb843e5ea37d7ee1e00c9d528
scseong <goidtv@naver.com>
2025-12-03 21:27:51 +0900

    chore: 로딩 스피너 이미지 변경

--- src/app/_components/layout/loading/Loading.tsx
+++ src/app/_components/layout/loading/Loading.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image';
 import React from 'react';
-import loadingBar from '../../../../../public/images/image_processing20210911-6827-1lzc0fb-ezgif.com-effects.gif';
+import loadingBar from '../../../../../public/images/loader.gif';
 import styles from './loading.module.scss';
 
 const Loading = () => {
   return (
     <div className={styles.container}>
-      <Image src={loadingBar} className={styles.image} alt="loading" quality={70} priority />
-      <p>퍼그가 정보를 가져오고 있습니다</p>
+      <Image src={loadingBar} className={styles.image} alt="loading" quality={30} priority />
     </div>
   );
 };
